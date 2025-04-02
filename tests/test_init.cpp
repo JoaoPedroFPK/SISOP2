4123f62ef33938c9ad96f03812dcff13f22935fe
Joao-Pereira <68359715+joaopfp@users.noreply.github.com>
2025-04-02 09:53:57 -0300

    Init Test

---
+++ tests/test_init.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../common/packet.h"
+#include "../client/sync.h"
+#include "../server/connection_handler.h"
+
+// Basic test initialization file
+// Add test framework initialization or common test utilities here
+
+int main() {
+    std::cout << "Test initialization complete" << std::endl;
+    return 0;
+}
