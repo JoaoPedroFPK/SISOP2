ccc98b039829e78a334bdc72218b21073c604840
luis <luisflf@icloud.com>
2025-06-20 15:15:10 -0300

    Update scripts

--- clear_tests.sh
+++ clear_tests.sh
@@ -1,6 +1,6 @@
-rm -rf test_servidor1
-rm -rf test_servidor2
-rm -rf test_cliente1
-rm -rf test_cliente2
-rm -rf test_fronted
-rm -rf test_frontend
+rm -v -rf test_servidor1
+rm -v -rf test_servidor2
+rm -v -rf test_cliente1
+rm -v -rf test_cliente2
+rm -v -rf test_fronted
+rm -v -rf test_frontend
--- setup_tests.sh
+++ setup_tests.sh
@@ -4,14 +4,14 @@ mkdir -p test_cliente1
 mkdir -p test_cliente2
 mkdir -p test_frontend
 
-rm -r test_cliente1/*
-rm -r test_cliente2/*
-rm -r test_servidor1/*
-rm -r test_servidor2/*
-rm -r test_frontend/*
+rm -v -r test_cliente1/*
+rm -v -r test_cliente2/*
+rm -v -r test_servidor1/*
+rm -v -r test_servidor2/*
+rm -v -r test_frontend/*
 
-cp server/primary_server test_servidor1/
-cp server/backup_server test_servidor2/
-cp client/client test_cliente1/
-cp client/client test_cliente2/
-cp frontend/frontend test_frontend/
+cp -v server/primary_server test_servidor1/
+cp -v server/backup_server test_servidor2/
+cp -v client/client test_cliente1/
+cp -v client/client test_cliente2/
+cp -v frontend/frontend test_frontend/
